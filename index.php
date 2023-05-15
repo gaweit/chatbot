ad57a51b4ada42dd300d600d2b7a34a8826d8384
GAWE IT <67357646+gaweit@users.noreply.github.com>
2023-05-15 23:59:15 +0700

    ok

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer Service Diskominfotik </title>
+    <title>Customer Service </title>
     <link rel="stylesheet" href="style.css">
     <link href="../assets/assets_home/img/logonav.png" rel="icon">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
@@ -13,7 +13,7 @@
 
 <body>
     <div class="wrapper">
-        <div class="title">Customer Service Diskominfotik</div>
+        <div class="title">Customer Service </div>
         <div class="form">
             <div class="bot-inbox inbox">
                 <div class="icon">
@@ -39,35 +39,35 @@
 </html>
 <script>
 $(document).ready(function() {
-    // jika tombol kirim 
+    // iko tombol kirim nyo
     $("#send-btn").on("click", function() {
-        // ambil inputan pesan 
+        // ambek pesan 
         $pesan = $("#text-pesan").val();
-        // tampung 
+        // tampung bae dulu
         $msg = '<div class="user-inbox inbox"><div class="msg-header">' + $pesan + '</div></div>';
-        // masukkan 
+        // masukkin 
         $(".form").append($msg);
-        // kosongkan 
+        // kosongin text kalo sudah kirim 
         $("#text-pesan").val('');
 
-        // ajax 
+        // ajax nyo ni
         $.ajax({
             url: 'pesan.php',
             type: 'POST',
             data: 'isi_pesan=' + $pesan,
             success: function(result) {
-                // jika sukses ambildata,tampung kedalam balasan 
+                // kalo sukses ngambek data,tampung kedalam balasan 
                 $balasan =
                     '<div class="bot-inbox inbox"> <div class="icon"> <i class="fas fa-user"></i> </div> <div class="msg-header"> <p>' +
                     result + ' </p> </div> </div>'
 
-                // masukkan kedalam form chat 
+                // masukkin kedalam form chat 
                 $(".form").append($balasan);
 
-                // buat otomatis scrolll jika ada pesan baru 
+                // buat otomatis scrolll kalo ado pesan baru 
                 $(".form").scrollTop($(".form")[0].scrollHeight);
             }
-
+            //Lukman 
         });
 
 
